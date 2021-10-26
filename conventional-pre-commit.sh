883ba4db7e1b82b8c615a0f186e5bbd2a9e10608
Daniel Voogsgerd <daniel@voogsgerd.nl>
2021-10-26 14:15:28 +0200

    feat: Change perl regex to extended regex

--- conventional-pre-commit.sh
+++ conventional-pre-commit.sh
@@ -21,16 +21,16 @@ msg_file=$1
 # join types with | to form regex ORs
 r_types="($(IFS='|'; echo "${types[*]}"))"
 # optional (scope)
-r_scope="(\([\w \/-]+\))?"
+r_scope="(\([[:alnum:] \/-]+\))?"
 # optional breaking change indicator and colon delimiter
 r_delim='!?:'
 # subject line, body, footer
-r_subject=" [\w][\s\S]+"
+r_subject=" [[:alnum:]].+"
 # the full regex pattern
 pattern="^$r_types$r_scope$r_delim$r_subject$"
 
-# check commit message
-if grep -Pq "$pattern" "$msg_file"; then
+# Check if commit is conventional commit
+if grep -Eq "$pattern" "$msg_file"; then
     exit 0
 fi
 
