038f7acec4057652d7e47c593e4281a2fb99292b
Tomas Sebestik <tomas.sebestik@espressif.com>
2023-06-06 10:58:48 +0200

    feat: Add rules for line length

--- conventional_pre_commit/format.py
+++ conventional_pre_commit/format.py
@@ -32,8 +32,20 @@ def r_delim():
 
 
 def r_subject():
-    """Regex str for subject line, body, footer."""
-    return r" .+"
+    """Regex str for subject line."""
+    # Description should be between 20 and 50 characters long
+    return r" .{20,50}"
+
+def r_subject():
+    """Regex str for subject line."""
+    # Description should start with an uppercase letter and be between 20 and 50 characters long
+    return r" [A-Z].{19,49}"
+
+
+def r_body_footer():
+    """Regex str for optional body and footer."""
+    # Each line should not exceed 100 characters
+    return r"(\n.{0,100})*"
 
 
 def conventional_types(types=[]):
@@ -51,7 +63,7 @@ def is_conventional(input, types=DEFAULT_TYPES):
     Optionally provide a list of additional custom types.
     """
     types = conventional_types(types)
-    pattern = f"^({r_types(types)}){r_scope()}{r_delim()}{r_subject()}$"
+    pattern = f"^({r_types(types)}){r_scope()}{r_delim()}{r_subject()}{r_body_footer()}$"
     regex = re.compile(pattern, re.DOTALL)
 
     return bool(regex.match(input))
