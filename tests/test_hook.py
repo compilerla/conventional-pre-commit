194edafa3ad37e85394666fefd5e7de3b11998e1
Kegan Maher <kegan@compiler.la>
2023-11-17 19:24:53 +0000

    refactor(tests): test main and subprocess.call separately

--- tests/test_hook.py
+++ tests/test_hook.py
@@ -2,7 +2,7 @@ import subprocess
 
 import pytest
 
-from conventional_pre_commit.hook import RESULT_SUCCESS, RESULT_FAIL, main
+from conventional_pre_commit.hook import RESULT_FAIL, RESULT_SUCCESS, main
 
 
 @pytest.fixture
@@ -46,6 +46,54 @@ def test_main_success__custom_conventional(conventional_commit_path):
     assert result == RESULT_SUCCESS
 
 
+def test_main_success__conventional_utf8(conventional_utf8_commit_path):
+    result = main([conventional_utf8_commit_path])
+
+    assert result == RESULT_SUCCESS
+
+
+def test_main_fail__conventional_gbk(conventional_gbk_commit_path):
+    result = main([conventional_gbk_commit_path])
+
+    assert result == RESULT_FAIL
+
+
+def test_main_fail__conventional_with_scope(conventional_commit_path):
+    result = main(["--force-scope", conventional_commit_path])
+
+    assert result == RESULT_FAIL
+
+
+def test_main_success__conventional_with_scope(cmd, conventional_commit_with_scope_path):
+    result = main(["--force-scope", conventional_commit_with_scope_path])
+
+    assert result == RESULT_SUCCESS
+
+
+def test_main_success__fixup_commit(fixup_commit_path):
+    result = main([fixup_commit_path])
+
+    assert result == RESULT_SUCCESS
+
+
+def test_main_fail__fixup_commit(fixup_commit_path):
+    result = main(["--strict", fixup_commit_path])
+
+    assert result == RESULT_FAIL
+
+
+def test_main_success__conventional_commit_multi_line(conventional_commit_multi_line_path):
+    result = main([conventional_commit_multi_line_path])
+
+    assert result == RESULT_SUCCESS
+
+
+def test_main_fail__conventional_commit_bad_multi_line(conventional_commit_bad_multi_line_path):
+    result = main([conventional_commit_bad_multi_line_path])
+
+    assert result == RESULT_FAIL
+
+
 def test_subprocess_fail__missing_args(cmd):
     result = subprocess.call(cmd)
 
@@ -82,55 +130,37 @@ def test_subprocess_success__custom_conventional(cmd, conventional_commit_path):
     assert result == RESULT_SUCCESS
 
 
-def test_main_success__conventional_utf8(conventional_utf8_commit_path):
-    result = main([conventional_utf8_commit_path])
-
-    assert result == RESULT_SUCCESS
-
-
-def test_main_fail__conventional_gbk(conventional_gbk_commit_path):
-    result = main([conventional_gbk_commit_path])
-
-    assert result == RESULT_FAIL
-
-
-def test_main_fail__conventional_with_scope(cmd, conventional_commit_path):
+def test_subprocess_fail__conventional_with_scope(cmd, conventional_commit_path):
     result = subprocess.call((cmd, "--force-scope", conventional_commit_path))
 
     assert result == RESULT_FAIL
 
 
-def test_main_success__conventional_with_scope(cmd, conventional_commit_with_scope_path):
+def test_subprocess_success__conventional_with_scope(cmd, conventional_commit_with_scope_path):
     result = subprocess.call((cmd, "--force-scope", conventional_commit_with_scope_path))
 
     assert result == RESULT_SUCCESS
 
 
-def test_main_success__fixup_commit(cmd, fixup_commit_path):
+def test_subprocess_success__fixup_commit(cmd, fixup_commit_path):
     result = subprocess.call((cmd, fixup_commit_path))
 
     assert result == RESULT_SUCCESS
 
 
-def test_main_success__fail_commit(cmd, fixup_commit_path):
+def test_subprocess_fail__fixup_commit(cmd, fixup_commit_path):
     result = subprocess.call((cmd, "--strict", fixup_commit_path))
 
     assert result == RESULT_FAIL
 
 
-def test_main_success__conventional_commit_multi_line(cmd, conventional_commit_multi_line_path):
+def test_subprocess_success__conventional_commit_multi_line(cmd, conventional_commit_multi_line_path):
     result = subprocess.call((cmd, conventional_commit_multi_line_path))
 
     assert result == RESULT_SUCCESS
 
 
-def test_main_success__conventional_commit_bad_multi_line(cmd, conventional_commit_bad_multi_line_path):
+def test_subprocess_fail__conventional_commit_bad_multi_line(cmd, conventional_commit_bad_multi_line_path):
     result = subprocess.call((cmd, conventional_commit_bad_multi_line_path))
-
-    assert result == RESULT_SUCCESS
-
-
-def test_main_success__conventional_commit_bad_multi_line_strict(cmd, conventional_commit_bad_multi_line_path):
-    result = subprocess.call((cmd, "--strict", conventional_commit_bad_multi_line_path))
 
     assert result == RESULT_FAIL
